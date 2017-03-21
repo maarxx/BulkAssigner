398f46fb76b6ee043423a9fdee6055a30f4d4f32
maarxx <chris.lewark@gmail.com>
2017-03-21 00:36:04 -0400

    Basic functionality

---
+++ BulkAssigner/MainTabWindow_BulkAssigner.cs
@@ -0,0 +1,121 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+
+namespace BulkAssigner
+{
+    public class MainTabWindow_BulkAssigner : MainTabWindow
+    {
+
+        private const float BUTTON_HEIGHT = 50f;
+        private const float BUTTON_SPACE = 10f;
+
+
+        public MainTabWindow_BulkAssigner()
+        {
+            //base.forcePause = true;
+        }
+
+        public override Vector2 InitialSize
+        {
+            get
+            {
+                //return base.InitialSize;
+                return new Vector2(250f, 400f);
+            }
+        }
+
+        public override MainTabWindowAnchor Anchor =>
+            MainTabWindowAnchor.Right;
+
+        public void setHostilityResponseMode(HostilityResponseMode hrm)
+        {
+            foreach (object o in Find.Selector.SelectedObjects)
+            {
+                if (o is Pawn)
+                {
+                    Pawn p = (Pawn) o;
+                    p.playerSettings.hostilityResponse = hrm;
+                }
+            }
+        }
+
+        public void setOutfit(Outfit outfit)
+        {
+            foreach (object obj in Find.Selector.SelectedObjects)
+            {
+                if (obj is Pawn)
+                {
+                    Pawn p = (Pawn) obj;
+                    p.outfits.CurrentOutfit = outfit;
+                }
+            }
+        }
+
+        public override void DoWindowContents(Rect canvas)
+        {
+            base.DoWindowContents(canvas);
+
+            List<FloatMenuOption> hostilityResponseOptions = new List<FloatMenuOption>();
+            foreach (HostilityResponseMode hrm in Enum.GetValues(typeof(HostilityResponseMode)))
+            {
+                hostilityResponseOptions.Add(new FloatMenuOption(hrm.ToString(), delegate { setHostilityResponseMode(hrm); }));
+            }
+
+            List<FloatMenuOption> outfitOptions = new List<FloatMenuOption>();
+            foreach (Outfit outfit in Current.Game.outfitDatabase.AllOutfits)
+            {
+                outfitOptions.Add(new FloatMenuOption(outfit.ToString(), delegate { setOutfit(outfit); }));
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                Rect nextButton = new Rect(canvas);
+                nextButton.y = i * (BUTTON_HEIGHT + BUTTON_SPACE);
+                nextButton.height = BUTTON_HEIGHT;
+
+                string buttonLabel;
+                switch (i)
+                {
+                    case 0:
+                        buttonLabel = "Set Enemy Response";
+                        if (Widgets.ButtonText(nextButton, buttonLabel))
+                        {
+                            Find.WindowStack.Add(new FloatMenu(hostilityResponseOptions));
+                        }
+                        break;
+                    case 1:
+                        buttonLabel = "Set Outfit";
+                        if (Widgets.ButtonText(nextButton, buttonLabel))
+                        {
+                            Find.WindowStack.Add(new FloatMenu(outfitOptions));
+                        }
+                        break;
+                    case 2:
+                        buttonLabel = "Current Colonist Shot Finished is: ";
+                        /* TODO: Implement
+                        if (Widgets.ButtonText(nextButton, buttonLabel))
+                        {
+                            component.colonistShotFinished = !curColonistShotFinished;
+                        }
+                        */
+                        break;
+                    case 3:
+                        buttonLabel = "Current Enemy Shot Begun is: ";
+                        /* TODO: Implement
+                        if (Widgets.ButtonText(nextButton, buttonLabel))
+                        {
+                            component.enemyShotBegun = !curEnemyShotBegun;
+                        }
+                        */
+                        break;
+                }
+            }
+        }
+
+    }
+}
