af549f5d1ff6413cff868d368174561749b2a0a1
maarxx <chris.lewark@gmail.com>
2020-11-23 23:39:10 -0500

    Port to ModButtons

---
+++ BulkAssigner/BulkAssigner_Functions.cs
@@ -0,0 +1,92 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+
+namespace BulkAssigner
+{
+    public static class BulkAssigner_Functions
+    {
+        public static void setHostilityResponseMode(HostilityResponseMode hrm)
+        {
+            foreach (object o in Find.Selector.SelectedObjects)
+            {
+                if (o is Pawn)
+                {
+                    Pawn p = (Pawn) o;
+                    p.playerSettings.hostilityResponse = hrm;
+                }
+            }
+        }
+
+        public static void setOutfit(Outfit outfit)
+        {
+            foreach (object obj in Find.Selector.SelectedObjects)
+            {
+                if (obj is Pawn)
+                {
+                    Pawn p = (Pawn) obj;
+                    p.outfits.CurrentOutfit = outfit;
+                    p.mindState.Notify_OutfitChanged();
+                }
+            }
+        }
+
+        public static void setDrugPolicy(DrugPolicy dp)
+        {
+            foreach (object obj in Find.Selector.SelectedObjects)
+            {
+                if (obj is Pawn)
+                {
+                    Pawn p = (Pawn )obj;
+                    p.drugs.CurrentPolicy = dp;
+                }
+            }
+        }
+
+        public static void setFoodRestriction(FoodRestriction fr)
+        {
+            foreach (object obj in Find.Selector.SelectedObjects)
+            {
+                if (obj is Pawn)
+                {
+                    Pawn p = (Pawn)obj;
+                    if (p?.foodRestriction?.CurrentFoodRestriction != null)
+                    {
+                        p.foodRestriction.CurrentFoodRestriction = fr;
+                    }
+                }
+            }
+        }
+
+        public static void dropEverythingFromInventory()
+        {
+            foreach (object obj in Find.Selector.SelectedObjects)
+            {
+                if (obj is Pawn)
+                {
+                    Pawn p = (Pawn)obj;
+                    p.inventory.DropAllNearPawn(p.Position);
+                }
+            }
+        }
+
+        public static void setAllowedArea(Area a)
+        {
+            foreach (object obj in Find.Selector.SelectedObjects)
+            {
+                if (obj is Pawn)
+                {
+                    Pawn p = (Pawn)obj;
+                    if (p?.playerSettings != null)
+                    {
+                        p.playerSettings.AreaRestriction = a;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ BulkAssigner/BulkAssigner_RegisterToMainTab.cs
@@ -0,0 +1,105 @@
+﻿using ModButtons;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
+
+namespace BulkAssigner
+{
+    [StaticConstructorOnStartup]
+    class Main
+    {
+        static Main()
+        {
+            var harmony = new Harmony("com.github.harmony.rimworld.maarx.bulkassigner");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+    [HarmonyPatch(typeof(MainTabWindow_ModButtons))]
+    [HarmonyPatch("DoWindowContents")]
+    class Patch_MainTabWindow_ModButtons_DoWindowContents
+    {
+        static void Prefix(MainTabWindow_ModButtons __instance, ref Rect canvas)
+        {
+            BulkAssigner_RegisterToMainTab.ensureMainTabRegistered();
+        }
+    }
+
+    class BulkAssigner_RegisterToMainTab
+    {
+        public static bool wasRegistered = false;
+
+        public static void ensureMainTabRegistered()
+        {
+            if (wasRegistered) { return; }
+
+            Log.Message("Hello from BulkAssigner_RegisterToMainTab ensureMainTabRegistered");
+
+            List<List<ModButton_Text>> columns = MainTabWindow_ModButtons.columns;
+
+            List<FloatMenuOption> hostilityResponseOptions = new List<FloatMenuOption>();
+            foreach (HostilityResponseMode hrm in Enum.GetValues(typeof(HostilityResponseMode)))
+            {
+                hostilityResponseOptions.Add(new FloatMenuOption(hrm.ToString(), delegate { BulkAssigner_Functions.setHostilityResponseMode(hrm); }));
+            }
+
+            List<FloatMenuOption> outfitOptions = new List<FloatMenuOption>();
+            foreach (Outfit outfit in Current.Game.outfitDatabase.AllOutfits)
+            {
+                outfitOptions.Add(new FloatMenuOption(outfit.label, delegate { BulkAssigner_Functions.setOutfit(outfit); }));
+            }
+
+            List<FloatMenuOption> drugPolicyOptions = new List<FloatMenuOption>();
+            foreach (DrugPolicy dp in Current.Game.drugPolicyDatabase.AllPolicies)
+            {
+                drugPolicyOptions.Add(new FloatMenuOption(dp.label, delegate { BulkAssigner_Functions.setDrugPolicy(dp); }));
+            }
+
+            List<FloatMenuOption> foodRestrictionOptions = new List<FloatMenuOption>();
+            foreach (FoodRestriction fr in Current.Game.foodRestrictionDatabase.AllFoodRestrictions)
+            {
+                foodRestrictionOptions.Add(new FloatMenuOption(fr.label, delegate { BulkAssigner_Functions.setFoodRestriction(fr); }));
+            }
+
+            List<FloatMenuOption> allowedAreas = new List<FloatMenuOption>();
+            allowedAreas.Add(new FloatMenuOption("Unrestricted", delegate { BulkAssigner_Functions.setAllowedArea(null); }));
+            foreach (Area a in Find.CurrentMap.areaManager.AllAreas)
+            {
+                if (a.AssignableAsAllowed())
+                {
+                    allowedAreas.Add(new FloatMenuOption(a.Label, delegate { BulkAssigner_Functions.setAllowedArea(a); }));
+                }
+            }
+            List<ModButtons.ModButton_Text> buttons = new List<ModButtons.ModButton_Text>();
+            buttons.Add(new ModButton_Text("Set Enemy Response", delegate {
+                Find.WindowStack.Add(new FloatMenu(hostilityResponseOptions));
+            }));
+            buttons.Add(new ModButton_Text("Set Outfit", delegate {
+                Find.WindowStack.Add(new FloatMenu(outfitOptions));
+            }));
+            buttons.Add(new ModButton_Text("Set Drug Policy", delegate {
+                Find.WindowStack.Add(new FloatMenu(drugPolicyOptions));
+            }));
+            buttons.Add(new ModButton_Text("Set Food Restriction", delegate {
+                Find.WindowStack.Add(new FloatMenu(foodRestrictionOptions));
+            }));
+            buttons.Add(new ModButton_Text("Set Allowed Area", delegate {
+                Find.WindowStack.Add(new FloatMenu(allowedAreas));
+            }));
+            buttons.Add(new ModButton_Text("Drop Everything", delegate {
+                BulkAssigner_Functions.dropEverythingFromInventory();
+            }));
+
+            columns.Add(buttons);
+
+            wasRegistered = true;
+        }
+    }
+}
