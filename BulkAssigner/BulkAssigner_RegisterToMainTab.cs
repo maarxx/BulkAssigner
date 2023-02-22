845df53b6ab5b3d220e13dea196630330b7181cc
maarxx <chris.lewark@gmail.com>
2023-02-22 14:09:41 -0500

    Remove Bulk Surgery Button Until I Fix It

--- BulkAssigner/BulkAssigner_RegisterToMainTab.cs
+++ BulkAssigner/BulkAssigner_RegisterToMainTab.cs
@@ -117,15 +117,15 @@ namespace BulkAssigner
                     Find.WindowStack.Add(BulkAssigner_Functions.getBulkConsumeFromInventoryFloatMenu());
                 }
             ));
-            buttons.Add(new ModButton_Text(
-                delegate
-                {
-                    return "Bulk Operate Humanoids";
-                },
-                delegate {
-                    Find.WindowStack.Add(BulkAssigner_Functions.getBulkOperationFloatMenu());
-                }
-            ));
+            //buttons.Add(new ModButton_Text(
+            //    delegate
+            //    {
+            //        return "Bulk Operate Humanoids";
+            //    },
+            //    delegate {
+            //        Find.WindowStack.Add(BulkAssigner_Functions.getBulkOperationFloatMenu());
+            //    }
+            //));
 
             columns.Add(buttons);
 
